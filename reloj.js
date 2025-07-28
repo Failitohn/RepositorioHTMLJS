101da252b8a53c553d9096962560dbc5de7bc518
Failitohn <Alcoba361@gmail.com>
2025-07-28 12:49:02 -0600

    Update reloj.js

--- reloj.js
+++ reloj.js
@@ -1,11 +1,11 @@
-// reloj.js
 function actualizarReloj() {
   const ahora = new Date();
   const horas = String(ahora.getHours()).padStart(2, '0');
   const minutos = String(ahora.getMinutes()).padStart(2, '0');
   const segundos = String(ahora.getSeconds()).padStart(2, '0');
-  document.getElementById('reloj').textContent = `${horas}:${minutos}:${segundos}`;
+  const milisegundos = String(ahora.getMilliseconds()).padStart(3,'0');  
+  document.getElementById('reloj').textContent = `${horas}:${minutos}:${segundos}.${milisegundos}`;
 }
 
-setInterval(actualizarReloj, 1000);
+setInterval(actualizarReloj, 100);
 actualizarReloj();
